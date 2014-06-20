a1b35fcdb6820db34f379fc604cc80049003bd5e
adam <linyue@live.cn>
2014-06-20 17:12:04 +0800

    Update setup.js

--- src/setup.js
+++ src/setup.js
@@ -20,8 +20,8 @@ seajs.config({
 
     },
     paths: {
-        'src': window.location.protocol + '//res.xiaoman.cn/src',
-        'res': window.location.protocol + '//res.xiaoman.cn/res'
+        'src': window.location.protocol + '//res.xiaoman.cn/life.js/src',
+        'res': window.location.protocol + '//res.xiaoman.cn/life.js/res'
     },
 	alias: {
         'baseCss': 'res/css/base.css',
