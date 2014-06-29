d63b46eb8a8f84dac60001a062ceaae19784507f
linyue <linyue@live.cn>
2014-06-29 23:01:19 +0800

    add editor module

---
+++ src/ui/editor.js
@@ -0,0 +1,15 @@
+define(function (require, exports, module) {
+    /**
+     *
+     *
+     * @author linyue
+     * @time 14-6-29
+     */
+
+
+    "use strict";
+
+
+    module.exports = {};
+
+});
