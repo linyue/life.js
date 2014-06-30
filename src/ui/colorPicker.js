29024b5c851585c16e7c85397f9135590356aceb
linyue <linyue@live.cn>
2014-06-30 12:26:46 +0800

    add colorPicker

---
+++ src/ui/colorPicker.js
@@ -0,0 +1,15 @@
+define(function (require, exports, module) {
+    /**
+     *
+     *
+     * @author linyue
+     * @time 14-6-30
+     */
+
+
+    "use strict";
+
+
+    module.exports = {};
+
+});
