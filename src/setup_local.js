498363f5ddede4807ed867509e74e21503e5ce22
linyue <linyue@live.cn>
2014-11-25 15:51:11 +0800

    modify  localtion config

--- src/setup_local.js
+++ src/setup_local.js
@@ -20,8 +20,8 @@ seajs.config({
         versionGlobal: ''
     },
     paths: {
-        'src': 'http://res.xiaoman.cn/life.js/src',
-        'res': 'http://res.xiaoman.cn/life.js/res'
+        'src': 'https://res.xiaoman.cn/life.js/src',
+        'res': 'https://res.xiaoman.cn/life.js/res'
     },
 	alias: {
         'baseCss': 'res/css/base.css',
